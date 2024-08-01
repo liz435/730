79b78b971ad8626a4e939e4b3e4b676fbfcd9131
Zelong Li <87303807+liz435@users.noreply.github.com>
2024-08-01 19:05:08 -0400

    last commit on this version

--- src/App.tsx
+++ src/App.tsx
@@ -23,16 +23,30 @@ function App() {
         </a> */}
 
         <div className='container'>
+
         <div className='leftSide'>
 
       <h1>Zelong Li</h1>
 
+      <div>
+      <p className="read-the-docs">
+      Zelong Li is a designer and transdisciplinary artist based in New York City, seamlessly integrating technology and the arts. His diverse body of work ranges from immersive physical installations to innovative coding projects, reflecting his unique vision and technical prowess. With a keen eye for detail and a passion for pushing creative boundaries, Zelong's projects captivate audiences and foster new experiences. Fluent in Chinese, he brings a multicultural perspective to his work, enriching his artistic practice. Zelong's dynamic approach continues to inspire and innovate, making him a prominent figure in the contemporary art and design landscape.
+      </p>
+      </div>
+
+
+
       <div className="tag">
+        <div className='read-the-docs'>
+          <p>
+            Browse Project by Tags
+          </p>
+        </div>
 
         <div className='tag-container'>
 
         <button onClick={()=> handleButtonClick(0)}  className={clickedButtonIndex === 0 ? 'clicked' : ''}>
-          Science
+          Tangible
         </button>
 
         <button onClick={()=> handleButtonClick(1)}  className={clickedButtonIndex === 1 ? 'clicked' : ''}>
@@ -61,16 +75,11 @@ function App() {
         </p> */}
       </div>
 
-      <div>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
       </div>
 
       <div className='rightSide'>
-        sjakdpow2jq
+        section II
       </div>
 
       </div>
